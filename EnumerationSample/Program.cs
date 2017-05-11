58a314f56cd0b5d0ca9cd5ba618e4510a2cfc3a8
Reynier Ramos P <reynier.ramos.p@gmail.com>
2017-05-12 00:03:01 +0200

    For loop and Enumerator samples

---
+++ EnumerationSample/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using static System.Console;
+
+namespace EnumerationSample
+{
+    public enum TimeOfDay
+    {
+        Morning = 0,
+        Afternoon = 1,
+        Evening = 2
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteGreeting(TimeOfDay.Morning);
+        }
+
+        static void WriteGreeting(TimeOfDay timeOfDay)
+        {
+            switch (timeOfDay)
+            {
+                case TimeOfDay.Morning:
+                    // Get the enum string
+                    TimeOfDay time = TimeOfDay.Morning;
+                    WriteLine(time.ToString());
+                    break;
+                case TimeOfDay.Afternoon:
+                    WriteLine("Good Afternoon!");
+                    break;
+                case TimeOfDay.Evening:
+                    // Get the enum value from string
+                    TimeOfDay time2 = (TimeOfDay)Enum.Parse(typeof(TimeOfDay), "evening", true);
+                    WriteLine((int)time2);
+                    break;
+                default:
+                    WriteLine("Hello!");
+                    break;
+            }
+        }
+    }
+}
---
+++ ForLoop/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+
+namespace Wrox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This loop iterates through rows
+            for (int i = 0; i < 100; i+=10)
+            {
+                // This loop iterates through columns
+                for (int j = i; j < i + 10; j++)
+                {
+                    Write($" {j}");
+                }
+                WriteLine();
+            }
+        }
+    }
+}
